627125626f3c2916ff126dd4f9168dcfcb5be748
root <root@kvps-180-235-228-107.secure.ne.jp>
2012-07-27 16:47:16 +0900

    Optimize font size

--- app/Controller/SmabloController.php
+++ app/Controller/SmabloController.php
@@ -18,38 +18,45 @@ class SmabloController extends AppController {
     $this->modelClass = null;
     $message = '';
 
+    # 画像の指定
     $jpeg = "files/56a5570f.jpg";
+    # 画像の大きさを取得
     list( $max_width, $max_height ) = getimagesize( $jpeg );
 
     if ($this->request->data){
       //$message = Sanitize::stripAll(
       //  $this->request->data['text1']);
-        $message = $this->request->data['text1'];
+      # テキストエリアから取得
+      $message = $this->request->data['text1'];
 
+      # フォント指定
       $font = "/usr/local/share/fonts/mplus-1p-regular.ttf";
-      $font_size = 24;
 
+      # フォントサイズを最適化
+      $font_size = $this->optimize_font_size( 48, $font, $message, $max_width );
     
       $str_img_size = imageftbbox( $font_size, 0, $font, $message );
       $width = $str_img_size[2] - $str_img_size[6];
       $height = $str_img_size[3] - $str_img_size[7];
 
+      # 文字列を書いただけの画像を作成
       $str_img = imagecreatetruecolor( $width + $font_size, $height + $font_size );
       $col = imagecolorallocate( $str_img, 255, 255, 255 );
 
       imagettftext( $str_img, $font_size, 0, $font_size / 2, $font_size + $font_size / 2, $col, $font, $message);
-      imagejpeg($str_img, "files/string.jpg");
+      imagejpeg($str_img, "files/string.jpg"); # 固定はやめよう。いつか失敗する
       
+      # オリジナルと文字列の画像を合体
       $orig_img = imagecreatefromjpeg($jpeg);
       $img = imagecreatetruecolor($max_width, $max_height + $height + $font_size);
       imagecopyresampled($img, $orig_img, 0, 0, 0, 0, $max_width, $max_height, $max_width, $max_height);
       imagecopyresampled($img,$str_img,$max_width / 2 - $width / 2 - $font_size,$max_height,0,0,$max_width,$max_height,$max_width,$max_height);
 
-      imagejpeg($img,"files/saved.jpg");
+      # マージした画像を生成
+      imagejpeg($img,"files/saved.jpg"); # 固定はやめる
 
-
+      # ビューに渡す文字列の生成
       $result = '<img src="/cake/files/saved.jpg" width="100%" style="max-width:' . $max_width . 'px;">';
-      $result .= '<br /><img src="/cake/files/string.jpg" width="100%" style="max-width:' . $width . 'px;">';
 
     } else {
       $result = '<img src="/cake/' . $jpeg . '" width="100%" style="max-width:' . $max_width . 'px;">';
@@ -58,6 +65,22 @@ class SmabloController extends AppController {
     $this->set("result", $result);
     $this->set("header_for_layout", "Sample");
     $this->set("footer_for_layout", "Copyright by Daisuke Ozaki. 2012");
+  }
+
+  private function optimize_font_size( $fsize, $font, $message, $max_width ) {
+
+      $font_size = $fsize;
+      $str_img_size = imageftbbox( $font_size, 0, $font, $message );
+      $width = $str_img_size[2] - $str_img_size[6];
+
+      while( $width >= $max_width )
+      {
+        # 20%縮めてみる
+        $font_size = (int) ( $font_size * 0.8 );
+        $str_img_size = imageftbbox( $font_size, 0, $font, $message );
+        $width = $str_img_size[2] - $str_img_size[6];
+      }
+      return $font_size;
   }
 }
 
